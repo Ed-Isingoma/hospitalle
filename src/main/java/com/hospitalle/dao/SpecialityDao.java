260e2cf27a9c2121eff76f9743e76484a311aba2
Edgar <eddiisingoma@gmail.com>
2025-06-25 10:14:40 +0300

    Add extra methods to the SpecialityDao GenericDao

--- src/main/java/com/hospitalle/dao/SpecialityDao.java
+++ src/main/java/com/hospitalle/dao/SpecialityDao.java
@@ -1,7 +1,37 @@
 package com.hospitalle.dao;
 
+import com.hospitalle.models.Auth;
 import com.hospitalle.models.Speciality;
+import com.hospitalle.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.Collections;
+import java.util.List;
 
 public class SpecialityDao extends GenericDao<Speciality> {
     public SpecialityDao(){ super(Speciality.class); }
+
+    public List<Speciality> findByDoctor(Auth user) {
+        try (Session s = HibernateUtil.getSession()) {
+            String hql = "FROM Speciality s WHERE s.doctor = :user";
+            Query<Speciality> q = s.createQuery(hql, Speciality.class);
+            q.setParameter("user", user);
+            return q.list();
+        } catch (HibernateException he) {
+            System.err.println("Error in findByDoctor: " + he.getMessage());
+            return Collections.emptyList();
+        }
+    }
+
+    public void saveAll(Auth user, List<Speciality> specs) {
+        try (Session s = HibernateUtil.getSession()) {
+            String hql = "FROM Speciality s WHERE s.doctor = :user";
+            Query<Speciality> q = s.createQuery(hql, Speciality.class);
+            q.setParameter("user", user);
+        } catch (HibernateException he) {
+            System.err.println("Error in saveAll: " + he.getMessage());
+        }
+    }
 }
