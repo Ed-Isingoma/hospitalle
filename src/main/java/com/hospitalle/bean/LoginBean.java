730a75cffbbf01920eb44cb286c769fe25bf1c91
Edgar <eddiisingoma@gmail.com>
2025-07-11 19:29:49 +0300

    Add completion logic for receptionist and login concerning the forms

--- src/main/java/com/hospitalle/bean/LoginBean.java
+++ src/main/java/com/hospitalle/bean/LoginBean.java
@@ -3,11 +3,12 @@ package com.hospitalle.bean;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serial;
 import java.io.Serializable;
 import com.hospitalle.models.Auth;
-import com.hospitalle.services.AuthService;
+import com.hospitalle.services.impl.AuthServiceImpl;
 
 @Named("loginBean")
 @SessionScoped
@@ -18,10 +19,11 @@ public class LoginBean implements Serializable {
     private String username;
     private String password;
 
-    private final AuthService authService = new AuthService();
+    @Inject
+    private AuthServiceImpl authServiceImpl;
 
     public String login() {
-        Auth user = authService.authenticate(username, password);
+        Auth user = authServiceImpl.authenticate(username, password);
         if (user == null) {
             FacesContext.getCurrentInstance()
                     .addMessage(null,
@@ -39,7 +41,7 @@ public class LoginBean implements Serializable {
                 .getSessionMap()
                 .put("user", user);
 
-        return authService.dashboardFor(user);
+        return authServiceImpl.dashboardFor(user);
     }
 
     public String getUsername() {
