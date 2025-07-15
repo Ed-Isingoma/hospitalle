a785c8d95546fc77137ac2da130ab2590d16d1b4
Edgar <eddiisingoma@gmail.com>
2025-07-15 08:25:08 +0300

    Interfaces made to replace concrete classes in services- 2

--- src/main/java/com/hospitalle/services/PatientService.java
+++ src/main/java/com/hospitalle/services/PatientService.java
@@ -1,52 +1,10 @@
 package com.hospitalle.services;
 
-import com.hospitalle.dao.AppointmentDao;
-import com.hospitalle.dao.AuthDao;
-import com.hospitalle.dao.SpecialityDao;
-import com.hospitalle.dto.AppointmentDto;
 import com.hospitalle.models.Auth;
-import com.hospitalle.models.Appointment;
 
-import java.util.ArrayList;
+import java.io.Serializable;
 import java.util.List;
 
-public class PatientService {
-    private final AppointmentDao appointmentDao = new AppointmentDao();
-    private final AuthDao authDao = new AuthDao();
-    private SpecialityDao specialityDao = new SpecialityDao();
-
-//    public List<AppointmentDto> getAppointmentsFor(Auth patient) {
-//        List<Appointment> raw = appointmentDao.findByPatient(patient);
-//        raw.add(null);
-//        List<AppointmentDto> appts = new ArrayList<>();
-//
-//        for (Appointment ap : raw) {
-//            AppointmentDto dto = new AppointmentDto();
-//            dto.setId(ap.getId());
-//            dto.setDoctor(ap.getSpeciality().getDoctor());
-//            dto.setPatient(patient);
-//            dto.setSymptoms(ap.getSymptoms());
-//            dto.setDiagnosis(ap.getDiagnosis());
-//            dto.setPrescription(ap.getPrescription());
-//            dto.setPatientFeedback(ap.getPatientFeedback());
-////            dto.setSpeciality(ap.getSpeciality());
-//            dto.setStartTime(ap.getAvailability().getStartTime());
-//            dto.setEndTime(ap.getAvailability().getEndTime());
-//
-//            appts.add(dto);
-//        }
-//        return appts;
-//    }
-
-    public List<Auth> getDoctors() {
-        return authDao.findByRoleWithSpecialities("doctor");
-    }
-
-    // public List<List<Speciality>> getSpecialitiesFor(List<Auth> doctors) {
-    //     List<List<Speciality>> specialities = new ArrayList<>();
-    //     for (Auth doc : doctors) {
-    //         specialities.add(specialityDao.findByDoctor(doc));
-    //     }
-    //     return specialities;
-    // }
+public interface PatientService extends Serializable {
+    List<Auth> getDoctors();
 }
--- src/main/java/com/hospitalle/services/ReceptionistService.java
+++ src/main/java/com/hospitalle/services/ReceptionistService.java
@@ -1,39 +1,21 @@
 package com.hospitalle.services;
 
-import com.hospitalle.dao.AppointmentDao;
-import com.hospitalle.dao.AdmissionDao;
-import com.hospitalle.dao.AuthDao;
-import com.hospitalle.dao.AvailabilityDao;
-import com.hospitalle.models.Appointment;
+import com.hospitalle.dto.AppointmentDto;
 import com.hospitalle.models.Admission;
 import com.hospitalle.models.Auth;
 import com.hospitalle.models.Availability;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class ReceptionistService {
-    private final AppointmentDao appointmentDao = new AppointmentDao();
-    private final AdmissionDao admissionDao = new AdmissionDao();
-    private final AuthDao authDao = new AuthDao();
-    private final AvailabilityDao availabilityDao = new AvailabilityDao();
+public interface ReceptionistService extends Serializable {
+    List<AppointmentDto> getPastAppointments();
 
+    List<AppointmentDto> getFutureAppointments();
 
-    public List<Appointment> getPastAppointments() {
-        return appointmentDao.findPastAppointments();
-    }
+    List<Admission> getAllAdmissions();
 
-    public List<Appointment> getFutureAppointments() {
-        return appointmentDao.findFutureAppointments();
-    }
-    public List<Admission> getAllAdmissions() {
-        return admissionDao.findAll();
-    }
+    List<Auth> getAllDoctors();
 
-    public List<Auth> getAllDoctors() {
-        return authDao.findByRole("doctor");
-    }
-
-    public List<Availability> getAllAvailabilities() {
-        return availabilityDao.findAll();
-    }
+    List<Availability> getAllAvailabilities();
 }
