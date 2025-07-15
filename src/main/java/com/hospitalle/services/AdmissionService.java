389e550f13a3360708bea7a6471faa0332c510e2
Edgar <eddiisingoma@gmail.com>
2025-07-15 08:20:40 +0300

    Interfaces made to replace concrete classes in services- 1

--- src/main/java/com/hospitalle/services/AdmissionService.java
+++ src/main/java/com/hospitalle/services/AdmissionService.java
@@ -1,58 +1,17 @@
 package com.hospitalle.services;
 
-import com.hospitalle.dao.AdmissionDao;
-import com.hospitalle.dao.AuthDao;
-import com.hospitalle.dto.AdmissionDto;
 import com.hospitalle.models.Admission;
 import com.hospitalle.models.Auth;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.io.Serializable;
 import java.util.List;
 
-public class AdmissionService {
-    private final AdmissionDao dao = new AdmissionDao();
-    private final AuthDao authDao = new AuthDao();
+public interface AdmissionService extends Serializable {
 
-    public List<AdmissionDto> findByPatient(Auth patient) {
-        List<Admission> adms = dao.findByPatient(patient);
-        return assignDto(adms);
-    }
+    List<Admission> findByPatient(Auth patient);
 
-    public Long newAdmission(LocalDateTime start, String ward, Long patientId) {
-        Auth patient = authDao.findById(patientId);
-        Admission adm = new Admission();
-        adm.setPatient(patient);
-        adm.setWard(ward);
-        adm.setStart_time(start);
-        return dao.save(adm);
-    }
+    void newAdmission(Admission ad);
 
-    protected List<AdmissionDto> assignDto(List<Admission> adms) {
-        List<AdmissionDto> dtos = new ArrayList<>();
+    void editAdmission(Admission adm);
 
-
-        for (Admission adm : adms) {
-            Integer bill;
-            if (adm.getPayment() != null ) {
-                bill = adm.getPayment().getAmount();
-            } else {
-                bill = null;
-            }
-
-            AdmissionDto dto = new AdmissionDto();
-            dto.setId(adm.getId());
-            dto.setBill(bill);
-            dto.setPaid(adm.getPayment() != null);
-            dto.setDischarge_date(adm.getDischarge_date());
-            dto.setWard(adm.getWard());
-            dto.setStart_time(adm.getStart_time());
-
-            dtos.add(dto);
-        }
-        return dtos;
-    }
-    public void editAdmission(Admission adm) {
-        dao.update(adm);
-    }
 }
--- src/main/java/com/hospitalle/services/AppointmentService.java
+++ src/main/java/com/hospitalle/services/AppointmentService.java
@@ -1,128 +1,26 @@
 package com.hospitalle.services;
 
-import com.hospitalle.dao.AppointmentDao;
-import com.hospitalle.dao.AvailabilityDao;
-import com.hospitalle.dao.SpecialityDao;
 import com.hospitalle.dto.AppointmentDto;
-import com.hospitalle.models.*;
+import com.hospitalle.models.Auth;
+import com.hospitalle.services.impl.AppointmentServiceImpl;
 
-import java.util.*;
-import java.time.LocalDateTime;
+import java.io.Serializable;
+import java.util.List;
 
-public class AppointmentService {
-    private final AvailabilityDao availDao = new AvailabilityDao();
-    private final AppointmentDao apptDao = new AppointmentDao();
-    private final SpecialityDao specDao = new SpecialityDao();
+public interface AppointmentService extends Serializable {
+    void newAppt(Long av, Long sp, Auth patient);
 
-    public static record AppointmentLists(
+    record AppointmentLists(
             List<AppointmentDto> pendingFuture,
             List<AppointmentDto> acceptedFuture,
             List<AppointmentDto> acceptedPast
     ) {}
 
-    public void newAppt(Long av, Long sp, Auth patient) {
-        Availability avail = availDao.findById(av);
-        Speciality spec = specDao.findById(sp);
+    AppointmentLists findAppointments(Auth user, boolean isDoctor);
 
-        Appointment appt = new Appointment();
-        appt.setPatient(patient);
-        appt.setSpeciality(spec);
-        appt.setAccepted(false);
-        apptDao.save(appt);
+    void addPatientFeedback(AppointmentDto apptDto);
 
-        avail.setAppointment(appt);
-        availDao.update(avail);
-    }
+    void addDoctorChanges(Long id, String diagnosis, String prescription);
 
-    public AppointmentLists findAppointments(Auth user, boolean isDoctor) {
-        try {
-            List<Availability> slots;
-            List<AppointmentDto> apptDtos = new ArrayList<>();
-
-            if (isDoctor) {
-                slots = availDao.findByDoctor(user);
-                for (Availability av : slots) {
-                    Appointment app = av.getAppointment();
-                    if (app == null) continue;
-                    AppointmentDto dto = assignDto(app, av);
-                    apptDtos.add(dto);
-                }
-            } else {
-                List<Appointment> appts = apptDao.findByParameters("Appointment", Map.of("patient", user));
-                appts.forEach(appt -> {
-                    apptDtos.add(assignDto(appt, appt.getAvailability()));
-                });
-            }
-
-            LocalDateTime now = LocalDateTime.now();
-            List<AppointmentDto> pendingFuture  = new ArrayList<>();
-            List<AppointmentDto> acceptedFuture = new ArrayList<>();
-            List<AppointmentDto> acceptedPast   = new ArrayList<>();
-
-//            List<Long> dtoFutureIds = new ArrayList<>();
-
-            for (AppointmentDto dto : apptDtos) {
-                boolean accepted = dto.getAccepted();
-                LocalDateTime start = dto.getStartTime();
-                if (!accepted && (!start.isBefore(now))) {
-                    pendingFuture.add(dto);
-                } else if (accepted && (!start.isBefore(now))) {
-                    acceptedFuture.add(dto);
-//                    dtoFutureIds.add(dto.getId());
-                } else if (accepted) {
-                    acceptedPast.add(dto);
-                }
-            }
-
-//            List<Payment> payments = apptDao.findPaymentsForAppts(dtoFutureIds);
-
-//            for (AppointmentDto dtoFuture : acceptedFuture) {
-//                boolean hasPayment = payments.stream()
-//                        .anyMatch(p -> p.getAppointment().getId().equals(dtoFuture.getId()));
-//
-//                dtoFuture.setPaid(hasPayment);
-//            }
-
-            return new AppointmentLists(pendingFuture, acceptedFuture, acceptedPast);
-        } catch (Exception e) {
-            System.out.println("Some error here: " + e.getMessage());
-//            e.printStackTrace();
-            return new AppointmentLists(
-                    new ArrayList<>(),
-                    new ArrayList<>(),
-                    new ArrayList<>()
-            );
-        }
-    }
-
-    public void addPatientFeedback(AppointmentDto apptDto) {
-        apptDao.addFeedback(apptDto.getId(), apptDto.getPatientFeedback());
-    }
-
-    public void addDoctorChanges(AppointmentDto apptDto) {
-        Appointment appt = new Appointment();
-        appt.setId(apptDto.getId());
-        appt.setDiagnosis(apptDto.getDiagnosis());
-        appt.setPrescription(apptDto.getPrescription());
-        appt.setAccepted(apptDto.getAccepted());
-
-        apptDao.update(appt);
-    }
-
-    protected AppointmentDto assignDto(Appointment appt, Availability av) {
-        AppointmentDto d = new AppointmentDto();
-        d.setId(appt.getId());
-        d.setDiagnosis(appt.getDiagnosis());
-        d.setPrescription(appt.getPrescription());
-        d.setPatientFeedback(appt.getPatientFeedback());
-        d.setPatient(appt.getPatient().getUsername());
-        d.setBill(appt.getSpeciality().getHourlyPrice());
-        d.setAccepted(appt.getAccepted());
-        d.setDoctor(appt.getSpeciality().getDoctor().getUsername());
-        d.setSpeciality(appt.getSpeciality().getName());
-        d.setStartTime(av.getStartTime());
-        d.setPaid(appt.getPayment() != null);
-        d.setEndTime(av.getEndTime());
-        return d;
-    }
+    void acceptAppointment(Long id);
 }
